6b646cc4cd6dc5f9de09799e4370fd506c6003ad
印聪 <cong.yin@ttpai.cn>
2018-09-19 16:43:06 +0800

    Initial Commit

---
+++ VideoPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayerDemo
+//
+//  Created by 印聪 on 2018/9/19.
+//  Copyright © 2018年 印聪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoPlayerDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  VideoPlayerDemo
+//
+//  Created by 印聪 on 2018/9/19.
+//  Copyright © 2018年 印聪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
